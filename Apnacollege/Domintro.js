ce8afad26df883116600b215e77c852818a75dc7
TarmanjotSingh <virktarman@gmail.com>
2026-05-07 19:37:53 +0000

    c

---
+++ Apnacollege/Domintro.js
@@ -0,0 +1,38 @@
+//why we link files when we can everything in one
+//file bca to imporve readability
+//and it increases modularity - we can reuse the code in other projects as well
+//u cann link as much as css or js files as u want in html file
+//html structure a, example ghar , bricks vgera
+// css style a, color kehda krna ghar ch
+//javascript logic a, switch board click kre te lights on off ho rhiya 
+//kehde button naal lights chldiya 
+
+            //linking files
+
+//<style>tag connects html with css</style>
+//<script> tag connects html with javascript</script>
+
+//jo v changes appa console ja elemetn ch krde a website 
+// te inspect dabbke oh temporary hunde page referesh kre te ht jande
+//file ch changes krne a permanent krne a te page refresh krke dekhna a 
+
+
+                    //window object 
+// refers to the browser window and provides properties and methods to interact with it.
+//  It is the global object in JavaScript, meaning that all global variables and functions are properties of the window object.
+//  For example, you can use window.alert() to display an alert message, or window.location to get or set the current URL of the page.
+
+                //why window a global object
+// for example if u write console.log.("hello")
+//and window.console.log("hello") or things like windows.alert("hello")
+//both will work because console is a property of the window object
+//so we can access console directly without mentioning window because its a global object
+
+                //Now what is dom
+//Document Object Model (DOM) is a programming interface for web documents.
+//  It represents the structure of a web page as a tree of objects, 
+// where each object corresponds to an element in the HTML document.
+//  The DOM allows developers to manipulate the content, structure, and style 
+// of a web page dynamically using JavaScript.
+//  With the DOM, you can access and modify elements, attributes,
+//  and text on a web page, as well as respond to user interactions such as clicks and keyboard events.
